2c35e63b3a1e6d8d5fe15834233a4b5f32f1c814
CallaLittle <wwestbury@SCO-480-002.shc.wisc.edu>
2016-02-24 04:04:12 -0600

    Activity 5 Completed
    Finished activity 5

--- js/main.js
+++ js/main.js
@@ -1,35 +1,75 @@
 /* Calla Little, 2016 */
 
-var map = L.map('map').setView([39, -97], 5);
+//create the map and set initial properties
+var map = L.map('map', {minZoom: 5}).setView([39, -97], 5);
 
+//get the tileset
 var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 	subdomains: 'abcd',
 	maxZoom: 19
 }).addTo(map);
 
+//create proportional symbols
 function createPropSymbols(data) {
 
 	var markerStyle = {
-		radius: 8,
-		fillColor: 'green',
-		color: 'black',
+		fillColor: 'blue',
+		//color: 'black',
 		weight: 1,
-		opacity: 1,
-		fillOpacity: 0.6
+		opacity: .3,
+		fillOpacity: .3
 	};
 
 	L.geoJson(data, {
 		pointToLayer: function(feature, latlng) {
-			console.log(feature);
-			return L.circleMarker(latlng, markerStyle);
+			
+			var attribute = "2008"
+			var atValue = Number(feature.properties[attribute]);
+			markerStyle.radius = calcRadius(atValue);
+
+			var popUp = '<p><b><center>' + feature.properties.City + '<br></center> ';
+			popUp += '<p>' + attribute + ':</b> ' + feature.properties[attribute] + '%';
+
+			var layer = L.circleMarker(latlng, markerStyle).bindPopup(popUp, {
+				offset: new L.Point(0, -markerStyle.radius),
+				closeButton: false
+			});
+
+			layer.on({
+				mouseover: function() {
+					this.openPopup();
+				},
+				mouseout: function() {
+					this.closePopup();
+				},
+				click: function() {
+					$('#panel').html(popUp);
+				}
+			})
+
+			return layer;
 		}
 	}).addTo(map);
 };
 
-$.ajax('data/PovertyRates08-14.geojson', {
-	dataType: 'json',
-	success: function(response) {
-		createPropSymbols(response) 
-	}
-});
+//determine the appropriate radius
+function calcRadius(atValue) {
+	var scaleFactor = 40;
+	return Math.sqrt((atValue * scaleFactor)/(Math.PI   * .9));
+}
+
+//get the data for the map
+function getData(map) {
+
+	$.ajax('data/PovertyRates08-14.geojson', {
+		dataType: 'json',
+		success: function(response) {
+			createPropSymbols(response) 
+		}
+	});
+
+};
+
+//intitialize the document
+$(document).ready(getData);
